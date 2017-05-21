eae58463a5b2779f4748ff6a2b77ed18459f7793
Mark St.Godard <markst.godard@boldcommerce.com>
2017-05-21 18:34:21 -0500

    simple php app

---
+++ lib/Range.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App;
+
+class Range
+{
+
+    public function __construct($start, $end)
+    {
+        $this->start = $start;
+        $this->end = $end;
+    }
+
+    /**
+     * Determines of two ranges overlap.
+     *
+     * @return true/false
+     */
+    public function overlaps($other)
+    {
+        // Check min/max of both and see if size of both fit
+        // |--- 10-20 ---|
+        //          |--- 15-25 ---|
+        // |======================|
+        // | 10   . . . . . . . 25|    min and max of both
+        // (25 - 10) <= (20 - 10) + (25 - 15) ?
+        //      (15) <= (10) + (10) ?
+        //
+        // |--- 10-20 ---|
+        //                |-21-25-|
+        // |======================|
+        // | 10   . . . . . . . 25|    min and max of both
+        // (25 - 10) <= (20 - 10) + (25 - 21) ?
+        //      (15) <= (10) + (4) ?
+
+        // get min and max
+        $min = $this->min($other);
+        $max = $this->max($other);
+
+        return ($max - $min) <= $this->size() + $other->size();
+    }
+
+    /**
+     * Return the smaller of two start ranges
+     */
+    private function min($other)
+    {
+        return $this->start < $other->start ? $this->start : $other->start;
+    }
+
+    /**
+     * Return the larger of two end ranges
+     */
+    private function max($other)
+    {
+        return $this->end > $other->end ? $this->end : $other->end;
+    }
+
+    /**
+     * Return the size of a range
+     */
+    private function size()
+    {
+        return $this->end - $this->start;
+    }
+
+}
---
+++ tests/RangeTest.php
@@ -0,0 +1,67 @@
+<?php
+
+use App\Range;
+
+class RangeTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testRangeOverlaps()
+    {
+        $r1 = new Range(10,20);
+        $r2 = new Range(15,25);
+
+        $this->assertTrue($r2->overlaps($r1));
+    }
+
+
+    public function testRangeDoesNotOverlap()
+    {
+        $r1 = new Range(10,20);
+        $r2 = new Range(21,25);
+
+        $this->assertFalse($r2->overlaps($r1));
+    }
+
+    public function testRangeThatIsAdjacent()
+    {
+        $r1 = new Range(10,20);
+        $r2 = new Range(20,25);
+
+        $this->assertTrue($r2->overlaps($r1));
+    }
+
+    /**
+     * @dataProvider nonoverlapping
+     */
+    public function testRangesThatDoNotOverlap($r1, $r2)
+    {
+        $this->assertFalse($r2->overlaps($r1));
+    }
+
+    /**
+     * @dataProvider overlapping
+     */
+    public function testRangesThatDoOverlap($r1, $r2)
+    {
+        $this->assertTrue($r2->overlaps($r1));
+    }
+
+    public function nonoverlapping()
+    {
+        return [
+            [new Range(10,20), new Range(21,200)],
+            [new Range(20,21), new Range(22,45)],
+            [new Range(1,20), new Range(100,200)]
+        ];
+    }
+
+
+    public function overlapping()
+    {
+        return [
+            [new Range(10,20), new Range(15,20)],
+            [new Range(1,200), new Range(199,200)],
+            [new Range(1000,2000), new Range(1500,1501)],
+        ];
+    }
+}
